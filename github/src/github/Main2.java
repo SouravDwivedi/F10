17b170148afaf5134878102d8e5617c9d3efd5d6
sdac-16 <sdac-16@sdac-16-PC>
2023-06-12 11:52:15 +0530

    this is 2.0

--- github/src/github/Main2.java
+++ github/src/github/Main2.java
@@ -1,7 +1,7 @@
 package github;
 
-public class Main {
+public class Main2 {
 public static void main(String[] args) {
-	System.out.println("thiuas is1.0");
+	System.out.println("this is 2.0");
 }
 }
